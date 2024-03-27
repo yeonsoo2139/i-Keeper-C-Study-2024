70fba933cc7874bd0a3c599ba1bb6ea5a6e8ea79
yeonsoo2139 <yeonsoo1003@gmail.com>
2024-03-27 16:24:49 +0900

    과제1주

--- test.c
+++ test.c
@@ -1,7 +1,76 @@
 #include <stdio.h>
+/*
+int main(void)
+{
+    printf("Hello");
+    return 0;
+}*/
+
+/*
+int main(void)
+{
+    printf("문장1 문장2");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    printf("Hello \nWorld");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    printf("\'Hello\'");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    printf("\"Hello World\"");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    printf("\"!@$%%^&*()\"");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    printf("\"C:\\Download\\hello.cpp\"");
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+
+    int n;
+    scanf("%d", &n);
+    printf("%d", n);
+
+    return 0;
+}
+*/
 
 int main(void)
 {
-    printf("Hello world!");
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", a + b + c);
+
     return 0;
 }
